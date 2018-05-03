23275b63fb34c8ae42090784287c15571eb1a65d
Alireza Khojastehfar <a.r.khojastehfar@gmail.com>
2018-05-03 14:37:41 +0430

    bug fix

--- peerConnected.sh
+++ peerConnected.sh
@@ -4,19 +4,23 @@ connectedNum=0
 gluster peer status > tmp
 while read LINE
 do
-	if [[ $LINE=="Number of Peers: 3" ]] ; then 
-		peerNumberFlag="true"
-	else
-		x=$(echo $LINE | tr "/n" "/n")
-		y=$(echo $x | awk '{print $1}')
-		z=$(echo $x | awk '{print $5}')
-		if [[ $y=="State:" && z=="(Connected)" ]] ; then 
-			connectedNum=$connectedNum+1
-		fi
-	fi
-done < tmp	
-if [[ $peerNumberFlag=="true" && $connectedNum=="3" ]] ; then 
-	echo "Connected"
+        if [[ $LINE == "Number of Peers: 3" ]]
+         then
+                peerNumberFlag="true"
+        else
+                echo "in else"
+                x=$(echo $LINE | tr "/n" "/n")
+                y=$(echo $x | awk '{print $1}')
+                z=$(echo $x | awk '{print $5}')
+                echo $y || $z
+                if [[ $y == "State:" && $z == "(Connected)" ]]
+                 then
+                        connectedNum=$(($connectedNum+1))
+                fi
+        fi
+done < tmp
+if [[ $peerNumberFlag=="true" && $connectedNum=="3" ]] ; then
+        echo "Connected"
 else
-	echo "DissConnected"
+        echo "DissConnected"
 fi
