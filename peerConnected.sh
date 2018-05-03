bb92c2292f48c74f29a84b95fa995e96ec74838f
Alireza Khojastehfar <a.r.khojastehfar@gmail.com>
2018-05-03 14:08:24 +0430

    first commit

---
+++ peerConnected.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+peerNumberFlag="false"
+connectedNum=0
+gluster peer status > tmp
+while read LINE
+do
+	if [[ $LINE=="Number of Peers: 3" ]] ; then 
+		peerNumberFlag="true"
+	else
+		x=$(echo $LINE | tr "/n" "/n")
+		y=$(echo $x | awk '{print $1}')
+		z=$(echo $x | awk '{print $5}')
+		if [[ $y=="State:" && z=="(Connected)" ]] ; then 
+			connectedNum=$connectedNum+1
+		fi
+	fi
+done < tmp	
+if [[ $peerNumberFlag=="true" && $connectedNum=="3" ]] ; then 
+	echo "Connected"
+else
+	echo "DissConnected"
+fi
